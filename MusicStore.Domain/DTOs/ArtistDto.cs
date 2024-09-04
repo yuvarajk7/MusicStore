4ae132f35eb2cd23873c191a63f4bb8a700de1b5
Yuvaraj <yuvaraj.kesavan@hotmail.com>
2024-09-03 21:11:15 -0500

    Verified HTTP Endpoint test for Album and Artist

--- MusicStore.Domain/DTOs/ArtistDto.cs
+++ MusicStore.Domain/DTOs/ArtistDto.cs
@@ -2,8 +2,10 @@ namespace MusicStore.Domain.DTOs;
 
 public class ArtistDto
 {
+    public int  Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Gender { get; set; }
     public string Country { get; set; }
+    public int AlbumId { get; set; }
 }
